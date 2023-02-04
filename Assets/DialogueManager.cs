09f5db4b2660a0caa392d2329fd9bfd3705d9617
donovan-westwater <donovan.westwater@gmail.com>
2023-02-04 13:21:26 -0800

    Finished with basic UI and dialogue selection system

--- Assets/DialogueManager.cs
+++ Assets/DialogueManager.cs
@@ -6,9 +6,10 @@ using UnityEngine;
 public class DialogueObject
 {
     public int event_id = -1;
-    public int personality_id = 0;
-    public int sentence_num = 0;
-    public Dictionary<int, List<string>> dialogues;
+    //public int personality_id = 0; //Represents active personality to use
+    //public int sentence_num = 0; //0 is always the prompt. All other sentences are choices
+    public Dictionary<int, List<string>> dialogues; //personality id is the key. prompt and choices are the value
+    //WIP Add list of effects assoiated with the dialogues
     public DialogueObject()
     {
         dialogues = new Dictionary<int, List<string>>();
@@ -17,18 +18,30 @@ public class DialogueObject
 }
 public class DialogueManager : MonoBehaviour
 {
+    public static DialogueManager instance;
+    public int currentPersonality = 0; //Temp value until we create a player
     [HideInInspector]
     public Dictionary<int, DialogueObject> dialogueDict;
+    [HideInInspector]
+    public List<DialogueObject> activeChoices;
     public TextAsset spreadsheet;
-    public delegate void OnDialogueUpdate();
-    public event OnDialogueUpdate onDialougeUpdateEvent;
-    bool wait = false;
+    public delegate void OnDialogueUpdateEvent();
+    public event OnDialogueUpdateEvent onDialougeUpdateEvent;
+    public bool wait = false;
     int turn = 0;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
         Random.InitState(System.DateTime.Now.Second);
         dialogueDict = new Dictionary<int, DialogueObject>();
+        activeChoices = new List<DialogueObject>();
         //Read from spreadsheet
         ReadCSV();
     }
@@ -38,33 +51,43 @@ public class DialogueManager : MonoBehaviour
         int tableSize = data.Length / 4 - 1;
         for(int i = 0; i < tableSize; i++)
         {
-            DialogueObject d = new DialogueObject();
-            d.event_id = System.Int32.Parse(data[4 * (i + 1)]);
-            d.personality_id = System.Int32.Parse(data[4 * (i + 1) + 1]);
-            d.sentence_num = System.Int32.Parse(data[4 * (i + 1) + 2]);
-            List<string> diaList;
-            if (d.dialogues.TryGetValue(d.personality_id, out diaList))
+            DialogueObject d; 
+            int id = System.Int32.Parse(data[4 * (i + 1)]);
+            if(!dialogueDict.TryGetValue(id,out d))
             {
-                diaList.Insert(d.sentence_num, data[4 * (i + 1) + 3] );
+                d = new DialogueObject();
+                d.event_id = id;
+                dialogueDict.Add(d.event_id, d);
+            }
+            int personality = System.Int32.Parse(data[4 * (i + 1) + 1]);
+            int sentence_num = System.Int32.Parse(data[4 * (i + 1) + 2]);
+            List<string> diaList;
+            if (d.dialogues.TryGetValue(personality, out diaList))
+            {
+                diaList.Insert(sentence_num, data[4 * (i + 1) + 3] );
             }
             else
             {
                 diaList = new List<string>();
                 diaList.Add(data[4 * (i + 1) + 3]);
-                d.dialogues.Add(d.personality_id, diaList);
+                d.dialogues.Add(personality, diaList);
             }
-            dialogueDict.Add(d.event_id, d);
+            
         }
     }
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         if (!wait)
         {
-            int i = Random.Range(0, 3);
-            Debug.Log("Dialogue "+dialogueDict[i].dialogues[0][0]);
+            //Pick new Events
+            int i = Random.Range(0, dialogueDict.Count);
+            activeChoices.Add(dialogueDict[i]);
+            onDialougeUpdateEvent();
+            //Debug.Log("Dialogue "+dialogueDict[i].dialogues[0][0]);
             wait = true;
+            turn++;
         }
-        if ((int)Time.time % 10 == 0) wait = false;
+
     }
 }
--- Assets/DiaogueUIManager.cs
+++ Assets/DiaogueUIManager.cs
@@ -1,15 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DiaogueUIManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject diaPrefab;
+    public GameObject promptWindow;
     void Start()
     {
-        
+        DialogueManager.instance.onDialougeUpdateEvent += OnDialougeUpdate;
     }
-
+    public void OnDialougeUpdate()
+    {
+        foreach (Transform t in transform)
+        {
+            t.GetComponent<Button>().onClick.RemoveAllListeners();
+            Destroy(t.gameObject);
+        }
+        DisplayChoices();
+    }
+    void DisplayChoices()
+    {
+        AddChoice(DialogueManager.instance.activeChoices[0]);
+    }
+    void AddChoice(DialogueObject d)
+    {
+        promptWindow.SetActive(true);
+        promptWindow.transform.GetChild(0).GetComponent<Text>().text = d.dialogues[DialogueManager.instance.currentPersonality][0];
+        for(int i =1;i< d.dialogues[DialogueManager.instance.currentPersonality].Count; i++)
+        {
+            GameObject uiObj = Instantiate(diaPrefab);
+            uiObj.gameObject.SetActive(true);
+            uiObj.GetComponent<Button>().onClick.AddListener(() => OnClickTest());
+            uiObj.transform.SetParent(transform, false);
+            uiObj.transform.GetChild(0).GetComponent<Text>().text = d.dialogues[DialogueManager.instance.currentPersonality][i];
+            
+        }
+    }
+    //Be able to select dialogue options. Doesn't do much else yet
+    public void OnClickTest()
+    {
+        DialogueManager.instance.activeChoices.RemoveAt(0);
+        DialogueManager.instance.wait = false;
+    }
     // Update is called once per frame
     void Update()
     {
